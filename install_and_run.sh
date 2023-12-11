763b48234c550ba4636a1ff5d8957a64e0589eb1
Ian Lasky <ianmlasky@gmail.com>
2023-12-11 14:20:51 -0500

    added an auto kill for the running process

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Check if the run_script_pid.txt file exists
+if [ -e run_script_pid.txt ]; then
+    # Read the PID from the file
+    pid=$(cat run_script_pid.txt)
+
+    # Kill the existing process
+    kill $pid
+    rm run_script_pid.txt  # Remove the PID file
+    echo "Background process stopped."
+fi
+
 # Create a virtual environment (if not already created)
 python3 -m venv venv
 
@@ -11,7 +22,7 @@ pip install -r requirements.txt
 
 # Prompt user for email and password
 read -p "Enter your email: " email
-# echo  # Add a newline
+echo  # Add a newline
 read -s -p "Enter your password: " password
 echo  # Add a newline
 
@@ -28,7 +39,10 @@ read -p "Enter the number of your preferred printer: " printer_number
 printer_name=$(lpstat -p -d | awk -v n="$printer_number" 'NR==n {print $2}')
 
 # Set the preferred printer as the default printer using lpoptions
-lpoptions -d "$printer_name" > /dev/null 2>&1 && echo "default printer set"
+lpoptions -d "$printer_name" > /dev/null 2>&1 && echo "Default printer set"
 
-# Run the run_script.sh in the background
-# ./run_script.sh &
+# Run the run_script.sh in the background and save the PID to a file
+./run_script.sh &
+
+# Save the PID to a file
+echo $! > run_script_pid.txt
