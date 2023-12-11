91cd1084194a60001b11e57cb175ac9d410eee08
Ian Lasky <ianmlasky@gmail.com>
2023-12-11 14:39:27 -0500

    added error file

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Log file path
+log_file="install_and_run.log"
+
+# Function to log errors
+log_error() {
+    echo "[ERROR] $1" >> "$log_file"
+}
+
 # Check if the run_script_pid.txt file exists
 if [ -e run_script_pid.txt ]; then
     # Read the PID from the file
@@ -12,13 +20,13 @@ if [ -e run_script_pid.txt ]; then
 fi
 
 # Create a virtual environment (if not already created)
-python3 -m venv venv
+python3 -m venv venv >> "$log_file" 2>&1
 
 # Activate the virtual environment
-source venv/bin/activate  # Assuming the virtual environment is in the 'venv' directory
+source venv/bin/activate  >> "$log_file" 2>&1
 
 # Install packages from requirements.txt
-pip install -r requirements.txt
+pip install -r requirements.txt >> "$log_file" 2>&1
 
 # Prompt user for email and password
 read -p "Enter your email: " email
@@ -38,10 +46,10 @@ read -p "Enter the number of your preferred printer: " printer_number
 printer_name=$(lpstat -p -d | awk -v n="$printer_number" 'NR==n {print $2}')
 
 # Set the preferred printer as the default printer using lpoptions
-lpoptions -d "$printer_name" > /dev/null 2>&1 && echo "Default printer set"
+lpoptions -d "$printer_name" > /dev/null 2>&1 && echo "Default printer set" || log_error "Failed to set default printer"
 
 # Run the run_script.sh in the background and save the PID to a file
-./run_script.sh &
+./run_script.sh >> "$log_file" 2>&1 &
 
 # Save the PID to a file
 echo $! > run_script_pid.txt
